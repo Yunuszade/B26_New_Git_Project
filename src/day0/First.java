586e3fd7623120cc5ffe9b4055d3430c1cd7daac
Sabuhi <sy.workmails@gmail.com>
2022-03-27 00:59:47 -0400

    file "First" was updated second time.

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,9 +1,11 @@
 package day0;
 
 public class First {
-    //TODO: tomorrow we have another class
-    //should be funny
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working...");
 
+    }
 
     //extra line 1
     //extra line 2
